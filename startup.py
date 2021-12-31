8fb0c611e283114073ad5831e7cf4504d3331796
bang173 <60353385+bang173@users.noreply.github.com>
2021-12-31 19:08:24 +0300

    Update startup.py

--- startup.py
+++ startup.py
@@ -3,11 +3,6 @@ import hikari
 
 import reko
 
-# Configures
-chat_id = 913906370770776104
-join_asset = 'reko/assets/join.jpg'
-me = 616691484057534465
-
 reko_activity = hikari.Activity(
     name='Привет, я Реко',
     type=hikari.ActivityType.COMPETING
@@ -21,43 +16,6 @@ if __name__ == '__main__':
         uvloop.install()
 
     reko = reko.Reko()
-
-    # @rekobot.listen(hikari.MemberCreateEvent)
-    # async def on_member_join(event: hikari.MemberCreateEvent):
-    #     channel = event.get_guild().get_channel(chat_id)
-    #     member_count = event.get_guild().member_count
-    #     embed = hikari.Embed(
-    #         title='Добро пожаловать!',
-    #         description='Не стесняйся, присаживайся к нам за наш котацу. 🍡'
-    #                     f'\nНас уже целых {member_count}!',
-    #         color=0xffe689
-    #     )
-    #     embed.set_image(join_asset)
-    #     await channel.send(
-    #         content=event.member.mention,
-    #         embed=embed,
-    #         user_mentions=[event.member.user]
-    #     )
-
-    # @rekobot.listen(hikari.GuildMessageCreateEvent)
-    # async def on_member_join_test(event: hikari.GuildMessageCreateEvent):
-    #     if event.content.startswith('reko test') and event.member.id == me:
-    #         channel = event.get_guild().get_channel(chat_id)
-    #         member_count = event.get_guild().member_count
-    #         embed = hikari.Embed(
-    #             title='Добро пожаловать!',
-    #             description='Не стесняйся, присаживайся к нам за наш котацу. 🍡'
-    #                         f'\nНас уже целых {member_count}!',
-    #             color=0xffe689
-    #         )
-
-    #         embed.set_image(join_asset)
-    #         # embed.set_thumbnail(event.member.ava)
-    #         await channel.send(
-    #             content=event.member.mention,
-    #             embed=embed,
-    #             user_mentions=[event.member.user]
-    #         )
 
     reko.load_extensions(
         'reko.bot.ext.other_cmds',
